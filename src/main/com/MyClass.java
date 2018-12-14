62353980e4066f762f6238bb983f96aa60852ac4
srikanth-josyula <44222500+srikanth-josyula@users.noreply.github.com>
2018-12-14 09:16:09 +0530

    Ant Sample Project

---
+++ src/main/com/MyClass.java
@@ -0,0 +1,8 @@
+package main.com;
+
+public class MyClass {
+    public static void main(String[] args) {
+    	 String msg = args[0];
+        System.out.println(msg+" !!");
+    }
+}
---
+++ src/main/com/sample.java
@@ -0,0 +1,7 @@
+package main.com;
+
+public class sample {
+public static void main(String[] args) {
+	System.out.println("ssß");
+}
+}
